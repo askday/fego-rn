406a43b6b1502fa2de2f6580ed471b9109fd163c
天天 <xiangbo-99@163.com>
2018-03-30 22:01:12 +0800

    0.0.10

--- components/ui/Dialog/tip.js
+++ components/ui/Dialog/tip.js
@@ -1,95 +1,138 @@
 /**
- * 提示框容器
+ * 简易的提示
+ * 基于模态实现，同一时刻只能创建一个提示框
  */
 import React from 'react';
-import { Text, ScrollView } from 'react-native';
-import { UIComponent } from 'common';
+import AppContainer from '../AppContainer';
+import TipAgent from './TipAgent';
 
-import Dialog from './index';
-
-export default class TipAgent extends UIComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      visible: true,
-    };
-  }
-  componentWillUnmount() {
-    this.close = () => { };
-  }
-  close(fn) {
-    this.setState({
-      visible: false,
-    }, fn);
-  }
-  _crtContent(style) {
-    const { content, title } = this.props;
-    if (typeof content === 'string') {
-      return <Text style={[style.content, title ? style.contentHasTitle : null]}>{content}</Text>;
-    }
-    return content;
-  }
-  render() {
-    const { style } = this;
-    const { title, actions, onAnimationEnd, type } = this.props;
-    const setAniEndFn = (endFn) => {
-      this.endFn = endFn;
-    };
-    const _onAniEnd = (visible) => {
-      if (onAnimationEnd) {
-        onAnimationEnd(visible);
-      }
-      // 动画关闭时回调
-      if (this.endFn && !visible) {
-        this.endFn(visible);
-      }
-    };
-    const footer = actions.map((button) => {
-      const orginPress = button.onPress || function temp() { };
-      button.onPress = () => {
-        const res = orginPress(button, setAniEndFn);
-        // 若返回false则不关闭
-        if (res !== false) {
-          this.close();
-          return;
-        }
-        // 若返回promise则resolve时才关闭
-        if (res && res.then) {
-          res.then(() => {
-            this.close();
-          });
-        }
-      };
-      return button;
-    });
-    const content = this._crtContent(style);
-    return (
-      <Dialog
-        title={title}
-        type={type}
-        visible={this.state.visible}
-        onClose={() => this.close}
-        footer={footer}
-        onAnimationEnd={_onAniEnd}
-      >
-        <ScrollView style={style.container} >
-          {content}
-        </ScrollView>
-      </Dialog>
-    );
-  }
-}
-TipAgent.baseStyle = {
-  container: {
-
+const defaultOption = {
+  alert: {
+    // title: '提示',
+    actions: [{ text: '确定', type: 'yes' }],
   },
-  content: {
-    fontSize: 16,
-    lineHeight: 22,
-    color: '#030303',
-    textAlign: 'center',
-  },
-  contentHasTitle: {
-    color: '#888',
+  confirm: {
+    // title: '请确认',
+    actions: [{ text: '取消', type: 'no', style: { color: '#858DA0' } }, { text: '确定', type: 'yes' }],
   },
 };
+const topViewDialogKey = 'modal.Dialog.tip';
+let curTip = null;
+const onAnimationEnd = (visible) => {
+  if (!visible) {
+    curTip = null;
+    AppContainer.remove(topViewDialogKey);
+  }
+};
+
+function creatTip(title, content, actions, type) {
+  actions = actions || defaultOption.alert.actions;
+  AppContainer.add(<TipAgent
+    key={topViewDialogKey}
+    ref={(el) => { curTip = el; }}
+    title={title}
+    type={type}
+    content={content}
+    actions={actions}
+    onAnimationEnd={onAnimationEnd}
+  />);
+}
+
+/**
+ * 基础的提示框（唯一不可叠加）
+ * @param {String} title
+ * @param {String/Element} content
+ * @param {Array} actions
+ */
+function tip(title, content, actions, type) {
+  if (curTip) {
+    curTip.close(() => {
+      setTimeout(() => {
+        AppContainer.remove(topViewDialogKey);
+        setTimeout(() => {
+          creatTip(title, content, actions);
+        }, 0);
+      }, 200);
+    });
+  } else {
+    creatTip(title, content, actions, type);
+  }
+}
+
+/**
+ * 提醒
+ * Dialog.alert('内容')
+ * Dialog.alert('内容', callback)
+ * Dialog.alert('标题', '内容')
+ * Dialog.alert('标题', '内容', callback)
+ * @param {String} title
+ * @param {String/Element} content
+ */
+function alert(title, content, callback, type) {
+  const opt = defaultOption.alert;
+  const argLen = arguments.length;
+  if (argLen === 1) {
+    content = title;
+    title = null;
+  }
+  if (argLen === 2) {
+    if (typeof content === 'function') {
+      callback = content;
+      content = title;
+      title = null;
+    }
+  }
+  title = title || opt.title;
+  if (opt.actions[0]) {
+    opt.actions[0].onPress = callback;
+  }
+  tip(title, content, opt.actions, type);
+}
+/**
+ * 确认
+ * Dialog.confirm('内容')
+ * Dialog.confirm('内容', callback)
+ * Dialog.confirm('标题', '内容')
+ * Dialog.confirm('标题', '内容', callback)
+ * @param {String} title
+ * @param {String/Element} content
+ */
+function confirm(title, content, callback, type) {
+  const opt = defaultOption.confirm;
+  const argLen = arguments.length;
+  if (argLen === 1) {
+    content = title;
+    title = null;
+  }
+  if (argLen === 2) {
+    if (typeof content === 'function') {
+      callback = content;
+      content = title;
+      title = null;
+    }
+  }
+  title = title || opt.title;
+  if (opt.actions[0]) {
+    opt.actions[0].onPress = callback;
+  }
+  if (opt.actions[1]) {
+    opt.actions[1].onPress = callback;
+  }
+  tip(title, content, opt.actions, type);
+}
+/**
+ * TipAgent容器，可设置基础样式
+ */
+tip.TipAgent = TipAgent;
+/**
+ * 设置默认配置
+ */
+tip.setDefaultOption = function setOptions(option) {
+  Object.assign(defaultOption, option);
+};
+export {
+  tip,
+  alert,
+  confirm,
+};
+export default tip;
